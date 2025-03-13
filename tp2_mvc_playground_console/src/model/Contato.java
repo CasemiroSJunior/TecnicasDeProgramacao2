6a5c3e7b07712e9d8646c5921c56b57f09a3acd2
CASEMIRO SENEME JUNIOR <casemiro.seneme@fatecararas.com>
2025-03-12 22:08:54 -0300

    Add initial implementation of contact management system with repository pattern

---
+++ tp2_mvc_playground_console/src/model/Contato.java
@@ -0,0 +1,62 @@
+package model;
+
+import java.util.Objects;
+
+public class Contato {
+    private Integer id;
+    private String name;
+    private String number;
+    private String email;
+
+    public Contato(Integer id, String name, String number, String email) {
+        this.id = id;
+        this.name = name;
+        this.number = number;
+        this.email = email;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Contato contato = (Contato) o;
+        return Objects.equals(id, contato.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
---
+++ tp2_mvc_playground_console/src/model/ContatoMySqlRepositoryImpl.java
@@ -0,0 +1,31 @@
+package model;
+
+import java.util.List;
+
+public class ContatoMySqlRepositoryImpl implements iContatoRepository{
+
+    @Override
+    public void salvar(Contato contato) {
+
+    }
+
+    @Override
+    public void atualizar(Contato contato) {
+
+    }
+
+    @Override
+    public void excluir(Integer id) {
+
+    }
+
+    @Override
+    public Contato buscarPorEmail(String email) {
+        return null;
+    }
+
+    @Override
+    public List<Contato> buscarTodos() {
+        return List.of();
+    }
+}
