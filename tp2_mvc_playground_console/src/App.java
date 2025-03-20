8c29667b26149e8101453abdca74b17564e59a10
CASEMIRO SENEME JUNIOR <casemiro.seneme@fatecararas.com>
2025-03-19 22:15:24 -0300

    Add database connection examples in main method for MySQL and MariaDB

--- tp2_mvc_playground_console/src/App.java
+++ tp2_mvc_playground_console/src/App.java
@@ -1,5 +1,24 @@
+import model.factory.ConnectionFactory;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws Exception {
+        /*Connection connectionMysql = ConnectionFactory.getConnection("mysql");
+        Connection connectionMariaDb = ConnectionFactory.getConnection("mariadb");
+        System.out.println(connectionMysql);
+        System.out.println(connectionMariaDb);
+
+        ResultSet rstMySQL = connectionMysql.createStatement().executeQuery("select version() as versao;");
+        ResultSet rstMariaDb = connectionMariaDb.createStatement().executeQuery("select version() as versao;");
+
+        if (rstMariaDb.next()){
+            System.out.println(rstMariaDb.getString("versao"));
+        }
+        if (rstMySQL.next()){
+            System.out.println(rstMySQL.getString("versao"));
+        }*/
+
     }
 }
