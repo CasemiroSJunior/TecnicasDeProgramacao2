f641b3ad345bb8340eda8c633a755a1fd887c1d0
Casemiro <casemiro.seneme@fatec.sp.gov.br>
2025-04-16 19:58:32 -0300

    Add initial project structure and MongoDB configuration files

---
+++ tp2suggestionbox/src/main/kotlin/br/com/fatecararas/tp2suggestionbox/Tp2suggestionboxApplication.kt
@@ -0,0 +1,11 @@
+package br.com.fatecararas.tp2suggestionbox
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Tp2suggestionboxApplication
+
+fun main(args: Array<String>) {
+	runApplication<Tp2suggestionboxApplication>(*args)
+}
---
+++ tp2suggestionbox/src/test/kotlin/br/com/fatecararas/tp2suggestionbox/Tp2suggestionboxApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.fatecararas.tp2suggestionbox
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Tp2suggestionboxApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
